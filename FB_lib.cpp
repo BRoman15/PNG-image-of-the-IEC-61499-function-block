391713dcc71a451bdc7336f779b94e4f5b6d75a9
BRoman15 <brandorfRA@yandex.ru>
2025-12-02 01:25:14 +0300

    1

--- FB_lib.cpp
+++ FB_lib.cpp
@@ -1,5 +1,5 @@
 #include "FB_lib.h"
-#include "pugixml.hpp"
+#include "pugixml-1.15/src/pugixml.hpp"
 #include <vector>
 #include <iostream>
 #include <iomanip>
@@ -46,14 +46,18 @@ class BasicFB{
             }
         }
 
-        void xml_unpacking(){
+
+        void get_main_attributes(){
             //Getting the name of the function block
             pugi::xpath_node_set elements_fb = doc.select_nodes("/FBType");
             for (pugi::xpath_node xpath_node : elements_fb) {
                 pugi::xml_node node = xpath_node.node();
                 name_FB = node.first_attribute().as_string();
             }
+            cout << name_FB;
+        }
 
+        void get_eventInputs_attributes(){
             //Get the attributes of the subnodes of node EventInputs
             pugi::xpath_node_set elements_input = doc.select_nodes("//EventInputs//Event");           
             for (pugi::xpath_node xpath_node : elements_input) {
@@ -76,30 +80,34 @@ class BasicFB{
                 }
                 eventInputs_attrebutes.push_back(event_input);
             }
+        }
 
+        void get_eventOutputs_attributes(){
             //Get the attributes of the subnodes of node EventOutput
             pugi::xpath_node_set elements_output = doc.select_nodes("//EventOutputs//Event");          
             for (pugi::xpath_node xpath_node : elements_output) {
                 pugi::xml_node node = xpath_node.node();
-                EventOutputs event_outpu; 
+                EventOutputs event_output; 
                 for (pugi::xml_attribute attribut : node.attributes()){
                     if ((string)attribut.name() == "Name"){
-                        event_outpu.name = attribut.as_string();
+                        event_output.name = attribut.as_string();
                     }
                     else if ((string)attribut.name() == "Type"){
-                        event_outpu.type = attribut.as_string();                        
+                        event_output.type = attribut.as_string();                        
                     }
                     else if ((string)attribut.name() == "Comment"){
-                        event_outpu.comment = attribut.as_string();                        
+                        event_output.comment = attribut.as_string();                        
                     }
                 }
                 if ((string)node.first_child().name() == "With"){
                     for (pugi::xml_node node_with : node.children())
-                        event_outpu.vars.push_back(node_with.first_attribute().as_string());                         
+                        event_output.vars.push_back(node_with.first_attribute().as_string());                         
                 }
-                eventOutputs_attrebutes.push_back(event_outpu);
+                eventOutputs_attrebutes.push_back(event_output);
             }
+        }
 
+        void get_inputVars_attributes(){
             //Get the attributes of the subnodes of node InputVars
             pugi::xpath_node_set elements_vinput = doc.select_nodes("//InputVars//*");          
             for (pugi::xpath_node xpath_node : elements_vinput) {
@@ -118,7 +126,9 @@ class BasicFB{
                 }
                 inputVars_attrebutes.push_back(vardeclaration_input);
             }
+        }
 
+        void get_outputVars_attributes(){
             //Get the attributes of the subnodes of node OutputVars
             pugi::xpath_node_set elements_voutput = doc.select_nodes("//OutputVars//*");          
             for (pugi::xpath_node xpath_node : elements_voutput) {
@@ -140,7 +150,11 @@ class BasicFB{
         }
 };
 
+class PNG_image{
+    
+};
+
 int main(){
     BasicFB a("test_data_xml//ADD_2.xml");
-    a.xml_unpacking();
+    a.get_main_attributes();
 }
