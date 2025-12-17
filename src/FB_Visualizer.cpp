2259533af2c971bcab2c2461d0314e359d83fe7e
BRoman15 <brandorfRA@yandex.ru>
2025-12-17 20:46:44 +0300

    fix

--- src/FB_Visualizer.cpp
+++ src/FB_Visualizer.cpp
@@ -83,29 +83,29 @@ void FB_Visualizer::rendering_FB(){
 
     // добавление var output
     top_margin = 25;
-    extra_length_connection = FB->get_countEvent_InputWith_var()*5;
+    extra_length_connection = FB->get_countEvent_OutputsWith_var()*5;
     for (OutputVars event : FB->get_outputVars_attrebutes()){
         image->addTriangle_Blue(x_FB + width_FB - image->get_text_size()/2, y_FB + top_margin);
         image->addText(event.name,
-            x_FB + width_FB - image->get_text_size() * 2 - 12,
+            x_FB + width_FB - image->get_width_text(event.name) - image->get_text_size()/2 - 10,
             y_FB + top_margin - 5);
         image->addLine(x_FB + width_FB,
             y_FB + top_margin + image->get_text_size()/2,
-            10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 10 + extra_length_connection);
+            10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 10 + extra_length_connection);
 
         image->addText(event.type,
-            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) +15,
+            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) +15,
             y_FB + top_margin - 5);
 
         if (event.comment.empty() == false){
             image->addLine(
-                x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 30,
+                x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 30,
                 y_FB + top_margin + image->get_text_size()/2,
                 5);
         }
 
         image->addText(event.comment,
-            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 45,
+            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 45,
             y_FB + top_margin - 5);
         
         var_for_connection[event.name] = y_FB + top_margin + image->get_text_size()/2;
@@ -124,12 +124,13 @@ void FB_Visualizer::rendering_FB(){
         image->addLine(x_FB,
             y_FB - height_event + top_margin + image->get_text_size()/2,
             -(10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 10 + extra_length_connection));
-
         if (event.vars.empty() == false){
             for (std::string var : event.vars){
-                image->addConnection(x_FB - 10 - image->get_size_square() - coefficient_order_connections,
-                    y_FB - height_event + top_margin + image->get_text_size()/2,
-                    var_for_connection[var] - (y_FB - height_event + top_margin + image->get_text_size()/2));
+                if (var_for_connection[var] != 0){
+                    image->addConnection(x_FB - 10 - image->get_size_square() - coefficient_order_connections,
+                        y_FB - height_event + top_margin + image->get_text_size()/2,
+                        var_for_connection[var] - (y_FB - height_event + top_margin + image->get_text_size()/2));
+                }
             }
             coefficient_order_connections += 15;
         }
@@ -157,7 +158,7 @@ void FB_Visualizer::rendering_FB(){
     for (EventOutputs event : FB->get_eventOutputs_attrebutes()){
         image->addTriangle_Green(x_FB + width_FB - image->get_text_size()/2, y_FB - height_event + top_margin);
         image->addText(event.name,
-            x_FB + width_FB - image->get_text_size() * 2 - 12,
+            x_FB + width_FB - image->get_width_text(event.name) - image->get_text_size()/2 - 10,
             y_FB - height_event + top_margin - 5);
 
         image->addLine(x_FB + width_FB,
@@ -166,25 +167,27 @@ void FB_Visualizer::rendering_FB(){
 
         if (event.vars.empty() == false){
             for (std::string var : event.vars){
+                if (var_for_connection[var] != 0){
                 image->addConnection(x_FB + width_FB + 10 + image->get_size_square() + coefficient_order_connections,
                     y_FB - height_event + top_margin + image->get_text_size()/2,
                     var_for_connection[var] - (y_FB - height_event + top_margin + image->get_text_size()/2));
+                }
             }
             coefficient_order_connections += 15;
         }
         image->addText(event.type,
-            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) +15,
+            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) +15,
             y_FB - height_event + top_margin - 5);
 
         if (event.comment.empty() == false){
             image->addLine(
-                x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 30,
+                x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 30,
                 y_FB - height_event + top_margin + image->get_text_size()/2,
                 5);
         }
 
         image->addText(event.comment,
-            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_InputWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 45,
+            x_FB + width_FB + (10 + image->get_size_square() * FB->get_countEvent_OutputsWith_var() + 5 + extra_length_connection) + image->get_width_text(event.type) + 45,
             y_FB - height_event + top_margin - 5);
         top_margin += 30;
     }
